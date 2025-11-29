7fefc5b1422c91ee3508395256208cb0872e4184
Monika Bošániová <monika.bosaniova@gmail.com>
2025-11-29 12:22:17 +0100

    buck walking anim

--- Assets/Scripts/BuckController.cs
+++ Assets/Scripts/BuckController.cs
@@ -3,22 +3,32 @@ using UnityEngine.InputSystem;
 
 public class BuckController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Vector2 moveInput;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Called automatically from PlayerInput component (Unity Events mode)
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        if (moveInput != Vector2.zero)
+        {
+            animator.enabled = true;
+        }
+        else
+        {
+            animator.enabled = false;
+        }
         if (moveInput.x > 0)
         {
             sr.flipX = true;
