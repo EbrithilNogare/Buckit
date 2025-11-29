ea6d536fca13b8282fb5ee4465b02420c7512071
Monika Bošániová <monika.bosaniova@gmail.com>
2025-11-29 23:23:53 +0100

    minigame setup

---
+++ Assets/Scripts/MinigameController.cs
@@ -0,0 +1,65 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class MinigameController : MonoBehaviour
+{
+    [SerializeField] GameObject minigameWindow;
+    [SerializeField] TextMeshProUGUI minigameText;
+    [SerializeField] Image progressBar;
+    
+    private string minigameChar1 = "E";
+    private string minigameChar2 = "Q";
+
+    private float minRadialValue = 0.1f;
+    private float maxRadialValue = 0.95f;
+    
+    private bool minigameActive;
+
+    private void Start()
+    {
+        RestartQuickTimeEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (progressBar.fillAmount >= maxRadialValue)
+        {
+            CallEnd();
+        }
+        if(minigameActive)
+            SetRadialValue(progressBar.fillAmount + (Time.deltaTime / 2));
+    }
+
+    private void CallEnd()
+    {
+        Debug.Log("DEATH!!!!");
+    }
+
+    public void MinigameStart()
+    {
+        minigameWindow.SetActive(true);
+        minigameActive = true;
+    }
+
+    public void SetMinigameText()
+    {
+        int random = Random.Range(0, 2);
+        minigameText.text = random == 0 ? minigameChar1 : minigameChar2;
+    }
+
+    private void RestartQuickTimeEvent()
+    {
+        SetRadialValue(0f);
+        SetMinigameText();
+        
+    }
+
+    private void SetRadialValue(float radialValue)
+    {
+        progressBar.fillAmount = Mathf.Clamp(radialValue, minRadialValue, maxRadialValue);
+    }
+}
