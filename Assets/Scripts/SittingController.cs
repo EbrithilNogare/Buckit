2dac9a06edf90c7c1562f8cb621af6c7045d5283
Monika Bošániová <monika.bosaniova@gmail.com>
2025-11-29 17:27:41 +0100

    meta, sprite swaping sitting

--- Assets/Scripts/SittingController.cs
+++ Assets/Scripts/SittingController.cs
@@ -1,16 +1,51 @@
+using System;
 using UnityEngine;
 
 public class SittingController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("---CONST---")] [SerializeField]
+    public int HitGained;
+    public Sprite SittingSprite0;
+    public Sprite SittingSprite1;
+    public Sprite SittingSprite2;
+    public Sprite SittingSprite3;
+
+    [Header("---DEBUG---")] [SerializeField]
+    private bool Hit; 
+
+    public Action OnSittingGainedDamage;
+    
+    private SpriteRenderer sr;
+
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        HitGained = 0;
+        OnSittingGainedDamage += SittingGainedDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        #region TEST
+
+        if (Hit)
+        {
+            OnSittingGainedDamage?.Invoke();
+            Hit = false;
+        }
+        #endregion
+    }
+
+    private void SittingGainedDamage()
+    {
+        HitGained++;
+        switch (HitGained)
+        {
+            case 0: sr.sprite = SittingSprite0; break;
+            case 1: sr.sprite = SittingSprite1; break;
+            case 2: sr.sprite = SittingSprite2; break;
+            case 3: sr.sprite = SittingSprite3; break;
+            default: sr.sprite = SittingSprite0; break;
+        }
     }
 }
