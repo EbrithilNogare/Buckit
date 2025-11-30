a16b5a72ca1fe823a6222f3539652147d4038842
Monika Bošániová <monika.bosaniova@gmail.com>
2025-11-30 16:23:45 +0100

    speed quicktime

--- Assets/Scripts/MinigameController.cs
+++ Assets/Scripts/MinigameController.cs
@@ -45,7 +45,7 @@ public class MinigameController : MonoBehaviour
 
         if (minigameActive)
         {
-            SetRadialValue(progressBar.fillAmount + (Time.deltaTime / 2f)); // moznost delit nejakou const
+            SetRadialValue(progressBar.fillAmount + (Time.deltaTime)); // moznost delit nejakou const
         }
     }
 
